45e6cc8ff544c1f8fa810be7076bce41807d816d
eWasteDev1776 <163363897+eWasteDev1776@users.noreply.github.com>
2024-06-24 16:10:19 -0500

    Attempted Apple silicon compatibility work

--- setup.py
+++ setup.py
@@ -1,4 +1,3 @@
-import os
 from setuptools import setup
 
 APP = ['SKUMaker.py']
@@ -7,8 +6,13 @@ OPTIONS = {
     'argv_emulation': False,
     'packages': ['PyQt5'],
     'includes': ['PyQt5.QtCore', 'PyQt5.QtGui', 'PyQt5.QtWidgets'],
-    'excludes': ['PyQt5.QtDesigner', 'PyQt5.QtNetwork', 'PyQt5.QtOpenGL', 'PyQt5.QtScript', 'PyQt5.QtSql', 'PyQt5.QtTest', 'PyQt5.QtXml'],
-    'iconfile': 'path/to/your/icon.icns',
+    'excludes': [
+        'PyQt5.QtDesigner', 'PyQt5.QtNetwork', 'PyQt5.QtOpenGL', 'PyQt5.QtScript', 
+        'PyQt5.QtSql', 'PyQt5.QtTest', 'PyQt5.QtXml',
+        'tkinter', 'unittest', 'email', 'html', 'http', 'xml',
+        'pydoc', 'doctest', 'argparse', 'datetime', 'zipfile',
+        'urllib', 'encodings', 'distutils', 'asyncio'
+    ],
     'plist': {
         'CFBundleName': 'SKUMaker',
         'CFBundleDisplayName': 'SKU Maker',
@@ -17,12 +21,16 @@ OPTIONS = {
         'CFBundleVersion': "0.1.0",
         'CFBundleShortVersionString': "0.1.0",
         'NSHumanReadableCopyright': u"Copyright © 2023, CityeWaste, All Rights Reserved"
-    }
+    },
+    'arch': 'universal2',
+    'optimize': 2,
 }
 
 setup(
+    name='SKUMaker',
     app=APP,
     data_files=DATA_FILES,
     options={'py2app': OPTIONS},
     setup_requires=['py2app'],
 )
+
